858d92f12956f87420834b209fb652eae2636f76
Shubham Saini <shubhamsaini86963@gmail.com>
2022-07-02 20:34:09 +0530

    first commit

---
+++ src/main/java/com/smart/Smartcontactmanager8Application.java
@@ -0,0 +1,13 @@
+package com.smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Smartcontactmanager8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Smartcontactmanager8Application.class, args);
+	}
+
+}
