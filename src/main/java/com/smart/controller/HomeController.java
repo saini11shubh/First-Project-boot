20c6f98b6e3dabec567811f1c52234a5225e052b
Shubham Saini <shubhamsaini86963@gmail.com>
2022-07-03 20:29:49 +0530

    2nd time change

---
+++ src/main/java/com/smart/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.smart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String Home(Model model) {
+		model.addAttribute("title","Home - Smart Contact Manager");
+		return "home";
+	}
+	
+	@GetMapping("/about")
+	public String About(Model model) {
+		model.addAttribute("title","About - Smart Contact Manager");
+		return "about";
+	}
+	
+	@RequestMapping("/signup")
+	public String signup(Model model) {
+		model.addAttribute("title","Register - Smart Contact Manager");
+		return "signup";
+	}
+}
